c257a6f8f79b4aa0be9b2fd492aeb47c36875353
blltrx <blltrx@roseis.gay>
2025-07-23 11:47:09 +0100

    new build commit

--- nixbuild.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-nix-shell -p python312 python312Packages.markdown --command ./build.sh
---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      # python-pkgs.pandas
+      # python-pkgs.requests
+      python-pkgs.markdown
+    ]))
+  ];
+}
